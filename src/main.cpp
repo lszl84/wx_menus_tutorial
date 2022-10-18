a4534699036f100df53e83e9f1983bc5d2466bca
Lukasz <lukasz.sromek@gmail.com>
2022-10-18 15:36:54 +0200

    predefined menu items

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@ class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+private:
+    void SetupMainMenu();
 };
 
 wxIMPLEMENT_APP(MyApp);
@@ -23,6 +26,34 @@ bool MyApp::OnInit()
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+    SetupMainMenu();
+
     this->SetMinSize(this->FromDIP(wxSize(200, 200)));
     this->SetSize(this->FromDIP(wxSize(800, 600)));
+}
+
+void MyFrame::SetupMainMenu()
+{
+    wxMenuBar *menuBar = new wxMenuBar();
+
+    wxMenu *fileMenu = new wxMenu();
+    fileMenu->Append(wxID_NEW);
+    fileMenu->Append(wxID_OPEN);
+    fileMenu->AppendSeparator();
+    fileMenu->Append(wxID_SAVE);
+    fileMenu->Append(wxID_SAVEAS);
+
+    wxMenu *editMenu = new wxMenu();
+
+    editMenu->Append(wxID_UNDO);
+    editMenu->Append(wxID_REDO);
+    editMenu->AppendSeparator();
+    editMenu->Append(wxID_CUT);
+    editMenu->Append(wxID_COPY);
+    editMenu->Append(wxID_PASTE);
+
+    menuBar->Append(fileMenu, "File");
+    menuBar->Append(editMenu, "Edit");
+
+    SetMenuBar(menuBar);
 }
